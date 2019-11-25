cd5b4ea0975dfe1aeaa55ed8300a585d65e87cef
Ivan <ivanl.g.27@hotmail.com>
2019-11-25 18:54:26 +0100

    Something

--- app/src/main/java/com/example/game/GameMenu.java
+++ app/src/main/java/com/example/game/GameMenu.java
@@ -5,6 +5,11 @@ package com.example.game;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 
 public class GameMenu extends AppCompatActivity {
 
@@ -12,6 +17,17 @@ public class GameMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button logIn = findViewById(R.id.logIn);
+        final Button signUp = findViewById(R.id.signUp);
+        final View.OnClickListener connectListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final int id = v.getId();
+              //  if (id==logIn) {
+                //}
+            }
 
+
+        };
     }
 }
